193e81b94a5735afd28f894ea7c3b250804a6eea
local.interloper <marinoljubetic8@gmail.com>
2023-11-09 16:38:39 +0100

    Reworked /about-me

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -22,113 +22,119 @@ import {EXPERIENCE} from "@/data/experience";
 import ExperienceCard from "@/app/about-me/experience-card";
 import classNames from "@/util/classNames";
 
+const TECHNOLOGIES = [
+  SiNextdotjs,
+  SiReact,
+  SiNodedotjs,
+  SiPostgresql,
+  SiElectron,
+  SiTauri,
+  SiGit,
+  SiDotnet,
+  SiLinux,
+  SiGodotengine,
+  SiUnity,
+  SiUnrealengine
+];
+
+const LANGUAGES = [
+
+  SiTypescript,
+  SiCsharp,
+  SiRust,
+  SiJavascript,
+  SiPython,
+  SiLua,
+  SiHaxe,
+  SiC,
+  TbSql,
+];
+
 const AboutMePage = () => {
   return (
-    <PageWrapper className="py-10">
-      <Container flow className="gap-5 px-5">
-        <Container responsive flow>
-          <Container className="h-full w-full bg-background rounded-xl p-5">
-            <Image src={ProfilePicture} alt="Profile Picture" className="h-32 w-32 rounded-full shadow" />
-            <TextWrapper>
-              <Text>local.interloper</Text>
-              <Text>Age: 23</Text>
-              <Text>Country: Croatia</Text>
-            </TextWrapper>
-          </Container>
-          <Container className="h-full w-full bg-background rounded-xl p-5">
-            <Title>About me</Title>
+    <PageWrapper className="pb-10">
+      <Container className="h-full w-full bg-background shadow p-5">
+        <Title>About me</Title>
+        <div className="flex gap-5 items-center">
+          <TextWrapper>
+            <Text>local.interloper</Text>
+            <Text>Age: 23</Text>
+            <Text>Country: Croatia</Text>
+          </TextWrapper>
+          <Image src={ProfilePicture} alt="Profile Picture" className="h-32 w-32 rounded-full shadow" />
+        </div>
+        <Container className="h-full p-2">
+          <TextWrapper>
             <Text>
               I am a 23 year old fella from Croatia that&apos;s been into programming for quite a while now. I have a
               bunch of personal and commercial projects under my belt at this point as programming is not only my
               career but also a hobby.
             </Text>
             <Text>
-              By nature I&apos;m one hella curious guy. This nudged me into developing a very diverse skill set and made
+              By nature I&apos;m one hella curious guy. This nudged me into developing a very diverse skill set and
+              made
               me very adaptable by nature.
             </Text>
-          </Container>
+          </TextWrapper>
         </Container>
-        <Container responsive flow className="h-full bg-background rounded-xl p-5">
-          <Container flow>
-            <Title>Languages:</Title>
-            <div className="flex flex-col justify-center h-full">
-              <div className="grid grid-cols-3 gap-2">
-                {[
-                  SiTypescript,
-                  SiCsharp,
-                  SiRust,
-                  SiJavascript,
-                  SiPython,
-                  SiLua,
-                  SiHaxe,
-                  SiC,
-                  TbSql,
-                ].map((Icon, i) => (
-                  <div
-                    key={i}
-                    className="flex items-center justify-center flex-col w-24 h-24 bg-background-light shadow rounded-xl"
-                  >
-                    <Icon size={40} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </Container>
-          <Container flow className="h-full">
-            <Title>Technologies:</Title>
-            <div className="flex flex-col justify-center h-full">
-              <div className="grid grid-cols-3 lg:grid-cols-4 gap-2">
-                {[
-                  SiNextdotjs,
-                  SiReact,
-                  SiNodedotjs,
-                  SiPostgresql,
-                  SiElectron,
-                  SiTauri,
-                  SiGit,
-                  SiDotnet,
-                  SiLinux,
-                  SiGodotengine,
-                  SiUnity,
-                  SiUnrealengine
-                ].map((Icon, i) => (
-                  <div
-                    key={i}
-                    className="flex items-center justify-center flex-col w-24 h-24 bg-background-light shadow rounded-xl"
-                  >
-                    <Icon size={40} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </Container>
-        </Container>
+      </Container>
 
-        <Container flow className="h-full bg-background rounded-xl p-5">
-          <Title>My personal projects</Title>
-          <Link
-            href="https://github.com/local-interloper/"
-            className={classNames(
-              "flex flex-col items-center justify-center bg-background-light hover:bg-background-dark rounded-xl"
-              ,"shadow h-32 w-32 hover:scale-110 transition-all duration-200"
-            )}
-            target="_blank"
-          >
-            <SiGithub size={80} />
-            <Text>Browse</Text>
-          </Link>
-          <Text>
-            As I&apos;ve already said, I do programming as a hobby too.
-            If you are interested into checking out some of my work. Feel free to check out my GitHub.
-          </Text>
-        </Container>
-        <Container flow className="h-full p-5">
-          <Title>Experience</Title>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-            {EXPERIENCE.map((experience, i) => <ExperienceCard key={i} experience={experience} />)}
+      <Container responsive flow className="h-full p-5">
+        <Container flow>
+          <Title>Languages:</Title>
+          <div className="flex flex-col justify-center h-full">
+            <div className="grid grid-cols-3 gap-2">
+              {LANGUAGES.map((Icon, i) => (
+                <div
+                  key={i}
+                  className="flex items-center justify-center flex-col w-24 h-24 bg-background-light shadow rounded-xl"
+                >
+                  <Icon size={40} />
+                </div>
+              ))}
+            </div>
           </div>
         </Container>
+        <Container flow className="h-full">
+          <Title>Technologies:</Title>
+          <div className="flex flex-col justify-center h-full">
+            <div className="grid grid-cols-3 lg:grid-cols-4 gap-2">
+              {TECHNOLOGIES.map((Icon, i) => (
+                <div
+                  key={i}
+                  className="flex items-center justify-center flex-col w-24 h-24 bg-background-light shadow rounded-xl"
+                >
+                  <Icon size={40} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </Container>
+      </Container>
 
+      <Container flow className="h-full bg-background p-5">
+        <Title>My personal projects</Title>
+        <Link
+          href="https://github.com/local-interloper/"
+          className={classNames(
+            "flex flex-col items-center justify-center bg-background-light hover:bg-background-dark rounded-xl"
+            , "shadow h-32 w-32 hover:scale-110 transition-all duration-200"
+          )}
+          target="_blank"
+        >
+          <SiGithub size={80} />
+          <Text>Browse</Text>
+        </Link>
+        <Text>
+          As I&apos;ve already said, I do programming as a hobby too.
+          If you are interested into checking out some of my work. Feel free to check out my GitHub.
+        </Text>
+      </Container>
+      <Container flow className="p-5">
+        <Title>Experience</Title>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          {EXPERIENCE.map((experience, i) => <ExperienceCard key={i} experience={experience} />)}
+        </div>
       </Container>
     </PageWrapper>
   );
