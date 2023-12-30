777cdbc4f03125c8d2aaba563fd2f0105085f51a
local.interloper <marinoljubetic8@gmail.com>
2023-12-30 02:02:16 +0100

    Removed recently updated

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -4,19 +4,9 @@ import {getGitHubRepos} from "@/util/apis/github";
 import RepoCard from "@/app/github/repo-card";
 
 const ProjectsPage = async () => {
-  const oneDay = 24 * 60 * 60 * 1000;
-
   const repos = (await getGitHubRepos())
     .filter(repo => !repo.fork)
     .sort((a, b) => new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime());
-
-  const recentlyUpdated = repos
-    .reduce((acc, cur) => (
-        ((Date.now() - new Date(cur.pushed_at).getTime()) / oneDay) < 5)
-        ? acc + 1
-        : acc
-      , 0
-    );
 
   return (
     <PageWrapper className="pt-20">
@@ -29,7 +19,6 @@ const ProjectsPage = async () => {
         <div className="stat max-w-sm">
           <div className="stat-title">Total public repos</div>
           <div className="stat-value">{repos.length}</div>
-          <div className="stat-desc">{recentlyUpdated} recent update{recentlyUpdated == 1 ? "" : "s"}</div>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
