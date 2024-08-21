2b6498d446ba92f186b02ebbe2b14bf4d59d358a
local-interloper <marinoljubetic8@gmail.com>
2024-08-21 09:38:28 +0200

    SEO Tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
 
 import netlify from "@astrojs/netlify";
@@ -14,5 +13,5 @@ export default defineConfig({
     applyBaseStyles: false
   }), mdx(), react(), icon()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
