652c08001b0b2a46988f2bc1f26346e2c05c268b
local-interloper <marinoljubetic8@gmail.com>
2024-08-21 10:02:14 +0200

    Added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://local.interloper.me",
   integrations: [tailwind({
     applyBaseStyles: false
-  }), mdx(), react(), icon()],
+  }), mdx(), react(), icon(), sitemap()],
   output: "server",
   adapter: vercel()
 });
